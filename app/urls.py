10e9e18a2a3c00e053302e13eed9ae98b471f535
Furkan Yılmaz <furkanyilmaz@Furkan-MacBook-Air.local>
2025-05-28 23:21:21 +0300

    product

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),  # http://localhost:8000/
+    path('products/', views.product_list, name='product_list'),
     
 ]
--- app/views.py
+++ app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 
@@ -7,3 +8,8 @@ def home(request):
 
 
     return render(request, 'index.html')
+
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'product_list.html', {'products': products})
