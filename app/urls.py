7bd2f8d3eaf490712de44a9aa9b50a69f2bef3c7
Furkan Yılmaz <furkanyilmaz@Furkan-MacBook-Air.local>
2025-05-29 00:36:36 +0300

    abc

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . import views
-
+from django.conf import settings
+from django.conf.urls.static import static
 urlpatterns = [
     path('', views.home, name='home'),  # http://localhost:8000/
     path('products/', views.product_list, name='product_list'),
     
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
