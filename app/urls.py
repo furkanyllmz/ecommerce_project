13e2b8f0596d455d4d9fb78cbfbcb7f4f6f08d9d
Furkan Yılmaz <furkanyilmaz@Furkan-MacBook-Air.local>
2025-05-29 01:37:41 +0300

    dildo

--- app/urls.py
+++ app/urls.py
@@ -5,6 +5,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.home, name='home'),  # http://localhost:8000/
     path('products/', views.product_list, name='product_list'),
+    path('products/<str:uid>/', views.product_detail, name='product_detail'),
+    path('cart/',     views.cart,           name='cart'),
     
 ]
 if settings.DEBUG:
--- app/views.py
+++ app/views.py
@@ -14,3 +14,32 @@ def product_list(request):
     products = Product.objects.all()
     print(products)
     return render(request, 'product_list.html', {'products': products})
+
+# app/views.py
+
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+
+
+def product_detail(request, uid):
+    # burada uid alanına göre ürünü alıyoruz
+    product = get_object_or_404(Product, uid=uid)
+    return render(request, 'detail.html', {
+        'product': product
+    })
+
+
+from django.shortcuts import render, get_object_or_404
+from .models import Product
+
+def product_list(request):
+    products = Product.objects.all()
+    return render(request, 'product_list.html', {'products': products})
+
+def product_detail(request, uid):
+    product = get_object_or_404(Product, uid=uid)
+    return render(request, 'detail.html', {'product': product})
+
+def cart(request):
+    # Sepet tümüyle JS ile yönetildiği için sadece boş bir sayfa render ediyoruz.
+    return render(request, 'cart.html')
