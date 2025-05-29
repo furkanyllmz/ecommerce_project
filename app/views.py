11d5a0bc9a3a2b4405b5a48ae0850fe3871702d9
Bekir Top <bekirtop@Bekir-MacBook-Air.local>
2025-05-29 03:38:44 +0300

    bitti

--- app/views.py
+++ app/views.py
@@ -1,45 +1,30 @@
 from django.shortcuts import render
-from .models import Product
-
-# Create your views here.
+from pymongo import MongoClient
+from django.conf import settings
 
 def home(request):
-
-
-
     return render(request, 'index.html')
 
-
 def product_list(request):
-    products = Product.objects.all()
-    print(products)
-    return render(request, 'product_list.html', {'products': products})
+    client = MongoClient(settings.MONGO_URI)
+    db = client['ecommerce']
+    collection = db['products']
+    products = list(collection.find())
 
-# app/views.py
+    # Her ürüne image_url ekle (eğer image varsa)
+    for p in products:
+        p['image_url'] = p.get('image', '')
 
-from django.shortcuts import render, get_object_or_404
-from .models import Product
-
-
-def product_detail(request, uid):
-    # burada uid alanına göre ürünü alıyoruz
-    product = get_object_or_404(Product, uid=uid)
-    return render(request, 'detail.html', {
-        'product': product
-    })
-
-
-from django.shortcuts import render, get_object_or_404
-from .models import Product
-
-def product_list(request):
-    products = Product.objects.all()
     return render(request, 'product_list.html', {'products': products})
 
 def product_detail(request, uid):
-    product = get_object_or_404(Product, uid=uid)
+    client = MongoClient(settings.MONGO_URI)
+    db = client['ecommerce']
+    collection = db['products']
+    product = collection.find_one({'uid': uid})
+    if product:
+        product['image_url'] = product.get('image', '')
     return render(request, 'detail.html', {'product': product})
 
 def cart(request):
-    # Sepet tümüyle JS ile yönetildiği için sadece boş bir sayfa render ediyoruz.
     return render(request, 'cart.html')
